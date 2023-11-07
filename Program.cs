954b4cda4571519f930cd1d643f49bb92327ac2b
guntupalli bala bhaskara rao <bala_guntupalli_bg@outlook.com>
2023-11-07 03:46:38 +0000

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "java home , dontet applciation, node js");
+app.MapGet("/", () => "java home , dontet applciation, node js, python");
 
 app.Run();
